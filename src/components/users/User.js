420531924669af762e906f4c6f510325edfd7a7e
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-12-04 12:06:08 +0100

    page styled to display relevant user info from api

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,27 +1,103 @@
-// import { useEffect } from "react";
+import { useEffect, Fragment } from "react";
+import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-// const User = ({ getUser, user, loading }) => {
-//   console.log(user);
-//   useEffect(() => {
-//     getUser(user.login);
-//   }, []);
+const User = ({ getUser, user, loading }) => {
+  console.log(user);
+  useEffect(() => {
+    getUser(user.login);
+  }, []);
 
-//   const {
-//     name,
-//     avatar_url,
-//     location,
-//     bio,
-//     blog,
-//     login,
-//     html_url,
-//     followers,
-//     following,
-//     public_repos,
-//     public_gists,
-//     hireable,
-//   } = user;
+  const {
+    name,
+    avatar_url,
+    location,
+    bio,
+    blog,
+    login,
+    html_url,
+    followers,
+    following,
+    public_repos,
+    public_gists,
+    hireable,
+    company,
+  } = user;
 
-//   return <div>User</div>;
-// };
+  User.propTypes = {
+    loading: PropTypes.bool.isRequired,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired,
+  };
 
-// export default User;
+  if (loading) return <Spinner />;
+
+  return (
+    <Fragment>
+      <Link to="/" className="btn btn-dark">
+        Back to search
+      </Link>
+      Hireable:{" "}
+      {hireable ? (
+        <i className="fas fa-check text-success" />
+      ) : (
+        <i className="fas fa-times-circle text-danger" />
+      )}
+      <div className="card grid-2">
+        <div className="all-center">
+          <img
+            src={avatar_url}
+            className="round-img"
+            alt=""
+            style={{ width: "150px" }}
+          />
+          <h1>{name}</h1>
+          <p>Location: {location}</p>
+        </div>
+        <div>
+          {bio && (
+            <Fragment>
+              <h3>Bio</h3>
+              <p>{bio}</p>
+            </Fragment>
+          )}
+          <a hrf={html_url} className="btn btn-dark my-1">
+            Visit GitHub profile
+          </a>
+          <ul>
+            <li>
+              {login && (
+                <Fragment>
+                  <strong>Username: </strong> {login}
+                </Fragment>
+              )}
+            </li>
+            <li>
+              {company && (
+                <Fragment>
+                  <strong>Company: </strong> {company}
+                </Fragment>
+              )}
+            </li>
+            <li>
+              {blog && (
+                <Fragment>
+                  <strong>Website: </strong> {blog}
+                </Fragment>
+              )}
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="card text-center">
+        <div className="badge badge-primary">Followers: {followers}</div>
+        <div className="badge badge-success">Following: {following}</div>
+        <div className="badge badge-light">Public Repos: {public_repos}</div>
+        <div className="badge badge-dark">Public Gists: {public_gists}</div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default User;

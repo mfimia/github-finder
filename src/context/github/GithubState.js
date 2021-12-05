446f9070828304989a501b5352837ff31c88df13
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-12-05 11:51:30 +0100

    deployment build

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -11,6 +11,17 @@ import {
   GET_REPOS,
 } from "../types";
 
+let githubClientId;
+let githubClientSecret;
+
+if (process.env.NODE_ENV !== "production") {
+  githubClientId = process.env.REACT_APP_GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.REACT_APP_GITHUB_CLIENT_SECRET;
+} else {
+  githubClientId = process.env.GITHUB_CLIENT_ID;
+  githubClientSecret = process.env.GITHUB_CLIENT_SECRET;
+}
+
 // We declare a global state that we are going to be able to use everywhere in the app
 const GithubState = (props) => {
   const initialState = {
@@ -32,7 +43,7 @@ const GithubState = (props) => {
     setLoading();
     axios
       .get(
-        `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        `https://api.github.com/search/users?q=${text}&client_id=${githubClientId}&client_secret=${githubClientSecret}`
       )
       .then((res) => {
         dispatch({
@@ -46,8 +57,8 @@ const GithubState = (props) => {
   const getUser = async (username) => {
     // setLoading();
     const res = await axios.get(
-      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
-      ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/users/${username}?client_id=${githubClientId}&client_secret=
+      ${githubClientSecret}`
     );
     dispatch({
       type: GET_USER,
@@ -61,7 +72,7 @@ const GithubState = (props) => {
     setLoading();
     const res = await axios.get(
       `https://api.github.com/users/${username}/repos?per_page=5&sort=created:adc&client_id=
-      ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      ${githubClientId}&client_secret=${githubClientSecret}`
     );
     dispatch({
       type: GET_REPOS,
